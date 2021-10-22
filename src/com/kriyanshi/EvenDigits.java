aa7e6c15bc599d68cffa7e843a915722b42b0de8
kriyanshi shah <kriyanshishah06@gmail.com>
2021-10-22 20:31:42 +0530

    Created Richest cutomer Wealth class

--- src/com/kriyanshi/EvenDigits.java
+++ src/com/kriyanshi/EvenDigits.java
@@ -3,8 +3,8 @@ package com.kriyanshi;
 public class EvenDigits {
     public static void main(String[] args) {
         int[] nums = {555,901,482,1771};
-//        System.out.println(digits(19329));
-        System.out.println(findNumbers(nums));
+        System.out.println(digits(-287478));
+//        System.out.println(findNumbers(nums));
     }
 
     static int findNumbers(int[] nums) {
@@ -27,7 +27,26 @@ public class EvenDigits {
         return numberOfDigits % 2 == 0;
     }
 
+    static int digit2(int num){
+        if(num < 0){
+            num *= -1;
+        }
+
+        if(num == 0){
+            return 1;
+        }
+
+        return (int)(Math.log10(num)) + 1;
+    }
+
     static int digits(int num){
+        if(num < 0){
+            num *= -1;
+        }
+
+        if(num == 0){
+            return 1;
+        }
         int count = 0;
         while(num > 0){
             count++;
---
+++ src/com/kriyanshi/richestCustomerWealth.java
@@ -0,0 +1,4 @@
+package com.kriyanshi;
+
+public class richestCustomerWealth {
+}
