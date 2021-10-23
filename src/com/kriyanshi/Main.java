fe97f6f4c69c2d3d81fccd4185ac0b9d1afaef47
kriyanshi shah <kriyanshishah06@gmail.com>
2021-10-24 00:35:37 +0530

    Initial commit on smallletestletter.java

--- src/com/kriyanshi/Main.java
+++
@@ -1,9 +0,0 @@
-package com.kriyanshi;
-//https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-
-    }
-}
