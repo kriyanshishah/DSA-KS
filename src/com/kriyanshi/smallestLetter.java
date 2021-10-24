f8998bf6a350a9d30c69083c8024e3a6fc50612c
kriyanshi shah <kriyanshishah06@gmail.com>
2021-10-24 20:27:58 +0530

    working smallestLetter.java

--- src/com/kriyanshi/smallestLetter.java
+++ src/com/kriyanshi/smallestLetter.java
@@ -11,11 +11,15 @@ public class smallestLetter {
     static char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
         int end = letters.length - 1;
-        while(start < end){
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
-            if(target < letters[mid]){
+
+            if (target < letters[mid]) {
                 end = mid - 1;
-            }else if(target > letters[mid]){
+            } else {
                 start = mid + 1;
             }
         }
