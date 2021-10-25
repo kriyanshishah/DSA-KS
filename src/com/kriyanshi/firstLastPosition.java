9eac37ab73a99ab00ff46cec816a206ae6730c08
kriyanshi shah <kriyanshishah06@gmail.com>
2021-10-25 11:56:45 +0530

    firstLastPosition.java creted and solved posOfElement.java created

--- src/com/kriyanshi/firstLastPosition.java
+++ src/com/kriyanshi/firstLastPosition.java
@@ -1,2 +1,39 @@
-package com.kriyanshi;public class firstLastPosition {
+package com.kriyanshi;
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+public class firstLastPosition {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] searchRange(int[] arr, int target) {
+        int[] position = new int[2];
+        position[0] = find(arr, target, true);
+        if(position[0] != -1){
+            position[1] = find(arr, target, false);
+        }
+        return position;
+    }
+
+     static int find(int[] arr, int target, boolean doLeft){
+        int start = 0;
+        int end = arr.length - 1;
+        int index = -1;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > target){
+                end = mid - 1;
+            }else if(arr[mid] < target){
+                start = end + 1;
+            }else {
+                index = mid;
+                if(doLeft){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return index;
+    }
 }
---
+++ src/com/kriyanshi/posOfElement.java
@@ -0,0 +1,5 @@
+package com.kriyanshi;
+
+public class posOfElement {
+    
+}
