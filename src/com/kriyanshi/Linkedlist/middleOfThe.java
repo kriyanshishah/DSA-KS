a54984c14e9b6243cafc5d10c0d23b37c2bae1f2
Kriyanshi Shah <kriyanshishah06@gmail.com>
2022-05-03 22:04:12 +0530

    added shortestUnsorted.java to Arrays folder

---
+++ src/com/kriyanshi/Linkedlist/middleOfThe.java
@@ -0,0 +1,28 @@
+package com.kriyanshi.Linkedlist;
+
+public class middleOfThe {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head, fast = head;
+            while(fast != null && fast.next!= null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
